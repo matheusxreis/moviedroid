c5780dc6b1b3ea2993f206b6c5a2c22b93986633
Matheus Reis <matheus.reis081@gmail.com>
2023-02-01 14:54:03 -0300

    feat: transform main activity in nav host

--- app/src/main/java/com/matheusxreis/moviedroid/MainActivity.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/MainActivity.kt
@@ -2,10 +2,31 @@ package com.matheusxreis.moviedroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        navController = findNavController(R.id.my_nav_host)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homeFragment
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
