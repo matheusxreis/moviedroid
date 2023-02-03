a171a32e455cced9e6423802c9cc83dbfa577411
Matheus Reis <matheus.reis081@gmail.com>
2023-02-03 11:31:47 -0300

    feat: created get trending method in network layer

--- app/src/main/java/com/matheusxreis/moviedroid/data/network/MovieAPI.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/data/network/MovieAPI.kt
@@ -1,4 +1,14 @@
 package com.matheusxreis.moviedroid.data.network
 
+import com.matheusxreis.moviedroid.models.MoviePoster
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.QueryMap
+
 interface MovieAPI {
+
+    @GET("trending")
+    fun getTrendingDay(
+        @QueryMap queries: Map<String, String>
+    ): Response<MoviePoster>
 }
--- app/src/main/java/com/matheusxreis/moviedroid/data/network/RemoteDataSource.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/data/network/RemoteDataSource.kt
@@ -3,6 +3,9 @@ package com.matheusxreis.moviedroid.data.network
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
-   //private val movieAPI: MovieAPI
+   private val movieAPI: MovieAPI
 ) {
+
+    fun getTrendingMovies(queries:Map<String, String>) = movieAPI.getTrendingDay(queries)
+
 }
