32850ae8655aa9f63d7316890bb63f7c46e38479
Matheus Reis <matheus.reis081@gmail.com>
2023-02-03 12:22:12 -0300

    refactor: change param function in remote data source

--- app/src/main/java/com/matheusxreis/moviedroid/data/network/RemoteDataSource.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/data/network/RemoteDataSource.kt
@@ -1,11 +1,13 @@
 package com.matheusxreis.moviedroid.data.network
 
+import okhttp3.MediaType
+import java.time.chrono.ChronoPeriod
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
    private val movieAPI: MovieAPI
 ) {
 
-    suspend fun getTrendingMovies(queries:Map<String, String>) = movieAPI.getTrendingDay(queries)
+    suspend fun getTrendingMovies(queries:Map<String, String>, mediaType: String, period: String) = movieAPI.getTrendingDay(queries, mediaType, period)
 
 }
