a21ba92f3adb80845a20fe39761711506b7ec4c2
Matheus Reis <matheus.reis081@gmail.com>
2023-02-03 12:32:13 -0300

    feat: created constants class

---
+++ app/src/main/java/com/matheusxreis/moviedroid/utils/Constants.kt
@@ -0,0 +1,14 @@
+package com.matheusxreis.moviedroid.utils
+
+import com.matheusxreis.moviedroid.BuildConfig
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL="https://api.themoviedb.org/3/"
+        val API_KEY= BuildConfig.apiKey
+
+    }
+
+}
