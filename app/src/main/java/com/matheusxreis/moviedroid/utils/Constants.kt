d3a5e28504dc1ff8b5d52b9a56fc6c647585deb7
Matheus Reis <matheus.reis081@gmail.com>
2023-02-03 12:54:10 -0300

    chore: added image base url in constants class

--- app/src/main/java/com/matheusxreis/moviedroid/utils/Constants.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/utils/Constants.kt
@@ -7,6 +7,7 @@ class Constants {
     companion object {
 
         const val BASE_URL="https://api.themoviedb.org/3/"
+        const val IMAGE_BASE_URL="https://image.tmdb.org/t/p/original"
         val API_KEY= BuildConfig.apiKey
 
     }
