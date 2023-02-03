ea89f6e6faff96d032b4d6af8ea0e7c4bbc8ce76
Matheus Reis <matheus.reis081@gmail.com>
2023-02-03 11:31:14 -0300

    feat: serialize movie poster model

--- app/src/main/java/com/matheusxreis/moviedroid/models/MoviePoster.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/models/MoviePoster.kt
@@ -1,7 +1,12 @@
 package com.matheusxreis.moviedroid.models
 
+import com.google.gson.annotations.SerializedName
+
 data class MoviePoster(
+    @SerializedName("poster_path")
     val imageUrl: String,
+    @SerializedName("id")
     val imdbId: String,
+    @SerializedName("original_title")
     val title: String
 )
