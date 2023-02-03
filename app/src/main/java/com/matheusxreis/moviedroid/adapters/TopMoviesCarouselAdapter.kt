2f3498d3be4f05f4ca1818ca613baf7d3d19c1e6
Matheus Reis <matheus.reis081@gmail.com>
2023-02-03 10:09:17 -0300

    feat: binding carousel item layout

--- app/src/main/java/com/matheusxreis/moviedroid/adapters/TopMoviesCarouselAdapter.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/adapters/TopMoviesCarouselAdapter.kt
@@ -9,6 +9,7 @@ import androidx.constraintlayout.helper.widget.Carousel
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.matheusxreis.moviedroid.R
+import com.matheusxreis.moviedroid.databinding.CarouselItemLayoutBinding
 import com.matheusxreis.moviedroid.models.MoviePoster
 import kotlinx.android.synthetic.main.carousel_item_layout.view.*
 
@@ -16,17 +17,22 @@ class TopMoviesCarouselAdapter(): RecyclerView.Adapter<TopMoviesCarouselAdapter.
 
     private var topMovies: List<MoviePoster> = emptyList()
 
-    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+    class MyViewHolder(private val binding: CarouselItemLayoutBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(topMovie:MoviePoster){
+            binding.moviePoster = topMovie
+            binding.executePendingBindings()
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.carousel_item_layout, parent, false)
-        return MyViewHolder(itemView)
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = CarouselItemLayoutBinding.inflate(layoutInflater, parent, false)
+        return MyViewHolder(binding)
 
     }
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val currentTopMovie = topMovies[position]
-        val imageView = holder.itemView.topMoviesImageView
-        imageView.load(currentTopMovie.imageUrl)
+        holder.bind(currentTopMovie)
     }
     override fun getItemCount(): Int = topMovies.size
     fun setData(newTopMovies: List<MoviePoster>){
