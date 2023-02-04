6a2d5457fcd509825c30aa5b377b740af7d905e5
Matheus Reis <matheus.reis081@gmail.com>
2023-02-04 10:11:11 -0300

    feat: created search result fragment

---
+++ app/src/main/java/com/matheusxreis/moviedroid/ui/fragments/SearchResultFragment.kt
@@ -0,0 +1,25 @@
+package com.matheusxreis.moviedroid.ui.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.matheusxreis.moviedroid.R
+
+class SearchResultFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search_result, container, false)
+    }
+
+}
