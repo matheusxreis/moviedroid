e4239df611e24e6260fb3bf9642aba2a00881981
Matheus Reis <matheus.reis081@gmail.com>
2023-02-04 14:07:39 -0300

    feat: added error drawable and crossfade in loadImageUrl

--- app/src/main/java/com/matheusxreis/moviedroid/bindingadapters/MoviesRowBinding.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/bindingadapters/MoviesRowBinding.kt
@@ -3,6 +3,7 @@ package com.matheusxreis.moviedroid.bindingadapters
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.load
+import com.matheusxreis.moviedroid.R
 import com.matheusxreis.moviedroid.utils.Constants
 
 class MoviesRowBinding {
@@ -12,7 +13,10 @@ class MoviesRowBinding {
         @BindingAdapter("loadImageUrl")
         @JvmStatic
         fun loadImageUrl(imageView: ImageView, url:String? = ""){
-            imageView.load("${Constants.IMAGE_BASE_URL}$url")
+            imageView.load("${Constants.IMAGE_BASE_URL}$url") {
+                crossfade(900)
+                error(R.drawable.no_result)
+            }
         }
     }
 
