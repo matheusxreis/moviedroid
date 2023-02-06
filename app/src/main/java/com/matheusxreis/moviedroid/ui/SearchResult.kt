ce59db8255d52ae5237e01f936bec8c62c137263
Matheus Reis <matheus.reis081@gmail.com>
2023-02-06 16:00:31 -0300

    feat: binding search activity and pass fragment layout to it

--- app/src/main/java/com/matheusxreis/moviedroid/ui/SearchResult.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/ui/SearchResult.kt
@@ -2,11 +2,28 @@ package com.matheusxreis.moviedroid.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.activity.viewModels
+import androidx.fragment.app.activityViewModels
 import com.matheusxreis.moviedroid.R
+import com.matheusxreis.moviedroid.bindingadapters.SearchResultBinding
+import com.matheusxreis.moviedroid.databinding.ActivitySearchResultBinding
+import com.matheusxreis.moviedroid.databinding.FragmentSearchResultBinding
+import com.matheusxreis.moviedroid.viewmodels.HomeViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SearchResult : AppCompatActivity() {
+
+    private val homeViewModel: HomeViewModel by viewModels<HomeViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_result)
+
+        val binding = ActivitySearchResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.homeViewModel = homeViewModel
+
+
     }
 }
