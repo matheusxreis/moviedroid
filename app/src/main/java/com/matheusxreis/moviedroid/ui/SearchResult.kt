49bc4cdae4451afc85305f2edf21d3e2f682b027
Matheus Reis <matheus.reis081@gmail.com>
2023-02-06 15:20:30 -0300

    feat: created search result activity

---
+++ app/src/main/java/com/matheusxreis/moviedroid/ui/SearchResult.kt
@@ -0,0 +1,12 @@
+package com.matheusxreis.moviedroid.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.matheusxreis.moviedroid.R
+
+class SearchResult : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_result)
+    }
+}
