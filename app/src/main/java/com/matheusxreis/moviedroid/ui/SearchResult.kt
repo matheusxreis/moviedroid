ab5494397640eac1c10dac2704e858931910f2c8
Matheus Reis <matheus.reis081@gmail.com>
2023-02-06 16:05:02 -0300

    feat: set up and populate rv in search result activity

--- app/src/main/java/com/matheusxreis/moviedroid/ui/SearchResult.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/ui/SearchResult.kt
@@ -1,29 +1,78 @@
 package com.matheusxreis.moviedroid.ui
 
+import MoviesAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import androidx.activity.viewModels
 import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.GridLayoutManager
 import com.matheusxreis.moviedroid.R
 import com.matheusxreis.moviedroid.bindingadapters.SearchResultBinding
 import com.matheusxreis.moviedroid.databinding.ActivitySearchResultBinding
 import com.matheusxreis.moviedroid.databinding.FragmentSearchResultBinding
+import com.matheusxreis.moviedroid.utils.NetworkResult
 import com.matheusxreis.moviedroid.viewmodels.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_search_result.*
 
 @AndroidEntryPoint
 class SearchResult : AppCompatActivity() {
 
     private val homeViewModel: HomeViewModel by viewModels<HomeViewModel>()
+    private lateinit var binding:ActivitySearchResultBinding
+    private val resultSearchAdapter: MoviesAdapter by lazy {
+        MoviesAdapter()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val binding = ActivitySearchResultBinding.inflate(layoutInflater)
+        binding = ActivitySearchResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.homeViewModel = homeViewModel
 
+        homeViewModel.search("laddxssddddt")
+        setUpRecyclerView()
+        populateRecyclerView()
 
+
+    }
+
+
+    private fun setUpRecyclerView(){
+
+        searchResultRv.adapter = resultSearchAdapter
+        searchResultRv.layoutManager = GridLayoutManager(this, 2)
+        showShimmer()
+    }
+
+    private fun populateRecyclerView(){
+        // homeViewModel.search()
+        homeViewModel.searchedResult.observe(this) {
+
+            when(it){
+                is NetworkResult.Error -> {
+                    hideShimmer()
+
+                }
+                is NetworkResult.Success ->{
+                    resultSearchAdapter.setData(it.data!!)
+                    hideShimmer()
+                }
+                is NetworkResult.Loading -> {
+                    showShimmer()
+                }
+            }
+
+
+        }
+    }
+
+    private fun showShimmer(){
+        searchResultRv.showShimmer()
+    }
+    private fun hideShimmer(){
+        searchResultRv.hideShimmer()
     }
 }
