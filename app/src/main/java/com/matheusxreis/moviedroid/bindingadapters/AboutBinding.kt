c9616a8e6dd9fbccee05ce8129d82abf3ba60e66
Matheus Reis <matheus.reis081@gmail.com>
2023-02-07 17:29:37 -0300

    feat: created genres binding adapter

--- app/src/main/java/com/matheusxreis/moviedroid/bindingadapters/AboutBinding.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/bindingadapters/AboutBinding.kt
@@ -2,6 +2,7 @@ package com.matheusxreis.moviedroid.bindingadapters
 
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.matheusxreis.moviedroid.models.Genre
 
 class AboutBinding {
     companion object {
@@ -10,6 +11,19 @@ class AboutBinding {
         @JvmStatic
         fun defineTextViewValue(textView: TextView, value:String?){
             textView.text = value
+        }
+
+        @BindingAdapter("getGenres")
+        @JvmStatic
+        fun getGenres(textView: TextView, genres:List<Genre>?){
+           var allGenres:String? = null
+           genres?.forEach {
+               if(!allGenres.isNullOrEmpty()){
+                   allGenres = "$allGenres, ${it.name}"
+               }
+               allGenres = it.name
+           }
+            textView.text = allGenres
         }
     }
 }
