9222c6f2461e273ab51b53b863efe824bd470005
Matheus Reis <matheus.reis081@gmail.com>
2023-02-06 21:39:56 -0300

    feat: added back pressed callback in details fragment

--- app/src/main/java/com/matheusxreis/moviedroid/ui/fragments/details/DetailsFragment.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/ui/fragments/details/DetailsFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.matheusxreis.moviedroid.R
 
 class DetailsFragment : Fragment() {
@@ -19,6 +22,15 @@ class DetailsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        class Back(enabled: Boolean) : OnBackPressedCallback(enabled) {
+            override fun handleOnBackPressed() {
+               findNavController().popBackStack()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, Back(true))
+
+
         return inflater.inflate(R.layout.fragment_details, container, false)
     }
 
