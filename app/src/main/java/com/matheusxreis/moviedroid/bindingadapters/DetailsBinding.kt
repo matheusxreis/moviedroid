04f1bd4f06c173f5c7e6ad6ac4c4d6ceb0290d01
Matheus Reis <matheus.reis081@gmail.com>
2023-02-06 22:30:49 -0300

    feat: created and applied load and blur image binding adapter

--- app/src/main/java/com/matheusxreis/moviedroid/bindingadapters/DetailsBinding.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/bindingadapters/DetailsBinding.kt
@@ -1,10 +1,18 @@
 package com.matheusxreis.moviedroid.bindingadapters
 
+import android.graphics.BlurMaskFilter
 import android.view.View
+import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import coil.load
 import com.matheusxreis.moviedroid.models.MoviePoster
+import androidx.lifecycle.Transformations
+import coil.transform.CircleCropTransformation
+import com.commit451.coiltransformations.BlurTransformation
+import com.matheusxreis.moviedroid.R
+import com.matheusxreis.moviedroid.utils.Constants
 
 class DetailsBinding {
     companion object {
@@ -19,6 +27,16 @@ class DetailsBinding {
                 is TextView -> {
                     view.text = "${moviePoster.vouteCount} votes"
                 }
+            }
+        }
+
+        @BindingAdapter("loadAndBlurImage")
+        @JvmStatic
+        fun loadAndBlurImage(imageView: ImageView, url: String){
+            imageView.load("${Constants.IMAGE_BASE_URL}$url")  {
+                transformations(BlurTransformation(imageView.context)) // You can add as many as desired
+                crossfade(900)
+                error(R.drawable.no_result)
             }
         }
     }
