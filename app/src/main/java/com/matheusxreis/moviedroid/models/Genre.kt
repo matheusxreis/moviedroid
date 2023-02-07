fb4c9bf7b092e7c50fa6310e06335dcac84eeb70
Matheus Reis <matheus.reis081@gmail.com>
2023-02-07 16:34:49 -0300

    feat: created movie details and genre models

---
+++ app/src/main/java/com/matheusxreis/moviedroid/models/Genre.kt
@@ -0,0 +1,10 @@
+package com.matheusxreis.moviedroid.models
+
+import com.google.gson.annotations.SerializedName
+
+data class Genre(
+    @SerializedName("id")
+    val id:Int,
+    @SerializedName("name")
+    val name:String
+)
---
+++ app/src/main/java/com/matheusxreis/moviedroid/models/MovieDetails.kt
@@ -0,0 +1,28 @@
+package com.matheusxreis.moviedroid.models
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieDetails(
+    @SerializedName("adult")
+    val adult:Boolean,
+    @SerializedName("backdrop_path")
+    val backdropImageUrl: String,
+    @SerializedName("poster_path")
+    val imageUrl: String?,
+    @SerializedName("genres")
+    val genres: List<Genre>,
+    @SerializedName("imdb_id")
+    val imdbId: String,
+    @SerializedName("overview")
+    val overview:String,
+    @SerializedName("status")
+    val status:String,
+    @SerializedName("vote_average")
+    val voteAverage: Float,
+    @SerializedName("vote_count")
+    val vouteCount: Number,
+    @SerializedName("release_date")
+    val realeasedAt: String,
+    @SerializedName("id")
+    val id: Number
+)
