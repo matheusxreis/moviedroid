9fa43dacd1203abd74529ca5ace9c3eec37af943
Matheus Reis <matheus.reis081@gmail.com>
2023-02-06 21:34:42 -0300

    feat: created go to details binding adapter

--- app/src/main/java/com/matheusxreis/moviedroid/bindingadapters/MoviesRowBinding.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/bindingadapters/MoviesRowBinding.kt
@@ -1,9 +1,12 @@
 package com.matheusxreis.moviedroid.bindingadapters
 
 import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
 import coil.load
 import com.matheusxreis.moviedroid.R
+import com.matheusxreis.moviedroid.ui.fragments.home.HomeFragmentDirections
 import com.matheusxreis.moviedroid.utils.Constants
 
 class MoviesRowBinding {
@@ -16,6 +19,16 @@ class MoviesRowBinding {
             imageView.load("${Constants.IMAGE_BASE_URL}$url") {
                 crossfade(900)
                 error(R.drawable.no_result)
+            }
+        }
+        @BindingAdapter("goToDetails")
+        @JvmStatic
+        fun goToDetails(imageView: ImageView, go:Boolean){
+            if(go){
+                imageView.setOnClickListener {
+                    val action = HomeFragmentDirections.actionHomeFragmentToDetailsFragment()
+                    imageView.findNavController().navigate(action)
+                }
             }
         }
     }
