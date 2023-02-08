a04b351e71f753db7bc97b342d8c12d007166ebc
Matheus Reis <matheus.reis081@gmail.com>
2023-02-08 13:28:34 -0300

    feat: created network listener

---
+++ app/src/main/java/com/matheusxreis/moviedroid/utils/NetworkListener.kt
@@ -0,0 +1,47 @@
+package com.matheusxreis.moviedroid.utils
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkCapabilities
+import android.os.Build
+import androidx.annotation.RequiresApi
+import kotlinx.coroutines.flow.MutableStateFlow
+
+class NetworkListener:ConnectivityManager.NetworkCallback() {
+
+    private val isNetworkAvailable = MutableStateFlow(false)
+
+    fun checkNetworkavailability(context: Context):MutableStateFlow<Boolean> {
+        val connectivityManager = context.getSystemService(
+            Context.CONNECTIVITY_SERVICE
+        ) as ConnectivityManager
+
+        connectivityManager.registerDefaultNetworkCallback(this)
+
+        var isConnected = false
+
+        connectivityManager.allNetworks.forEach {
+            network ->
+            val networkCapability = connectivityManager.getNetworkCapabilities(network)
+            networkCapability.let {
+                if(it!=null){
+                    if(it.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)){
+                        isConnected = true
+                        return@forEach
+                    }
+                }
+            }
+        }
+
+        isNetworkAvailable.value = isConnected
+        return isNetworkAvailable
+    }
+
+    override fun onAvailable(network: Network) {
+        isNetworkAvailable.value = true
+    }
+    override  fun onLost(network: Network){
+        isNetworkAvailable.value = false
+    }
+}
