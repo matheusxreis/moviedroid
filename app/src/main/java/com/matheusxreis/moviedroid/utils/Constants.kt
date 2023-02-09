9d77f407283c25f4a43c925e53145b17870ea84c
Matheus Reis <matheus.reis081@gmail.com>
2023-02-09 15:45:00 -0300

    feat: added youtub baseurl in constants

--- app/src/main/java/com/matheusxreis/moviedroid/utils/Constants.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/utils/Constants.kt
@@ -8,6 +8,7 @@ class Constants {
 
         const val BASE_URL="https://api.themoviedb.org/3/"
         const val IMAGE_BASE_URL="https://image.tmdb.org/t/p/original"
+        const val YOUTUBE_BASE_URL="https://www.youtube.com/watch?v="
         val API_KEY= BuildConfig.apiKey
 
     }
