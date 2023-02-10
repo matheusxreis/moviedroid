650524d37ed07b759cf07178e24e5e6a0d45a614
Matheus Reis <matheus.reis081@gmail.com>
2023-02-10 12:17:31 -0300

    feat: binding video row layout and populate recycler view

--- app/src/main/java/com/matheusxreis/moviedroid/ui/fragments/videos/VideosFragment.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/ui/fragments/videos/VideosFragment.kt
@@ -6,11 +6,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.matheusxreis.moviedroid.R
+import com.matheusxreis.moviedroid.adapters.VideosAdapter
+import com.matheusxreis.moviedroid.models.Video
 import kotlinx.android.synthetic.main.fragment_videos.*
 
 class VideosFragment : Fragment() {
 
+    private val videosAdapter by lazy {
+        VideosAdapter()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -25,8 +31,10 @@ class VideosFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setVideo()
+       // setVideo()
 
+        setUpRecyclerView()
+        populateRecyclerView()
     }
 
     override fun onDestroy() {
@@ -34,9 +42,29 @@ class VideosFragment : Fragment() {
         super.onDestroy()
 
     }
+
+
+    // CUSTOM FUNCTIONS
+
+    private fun setUpRecyclerView(){
+        videosRecyclerView.adapter = videosAdapter
+        videosRecyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+    }
+    private fun populateRecyclerView(){
+        val mockVideos = listOf(
+            Video(key="AkFqg5wAuFk", from="youtube", title="title"),
+            Video(key="AkFqg5wAuFk", from="youtube", title="title"),
+            Video(key="AkFqg5wAuFk", from="youtube", title="title"),
+            Video(key="AkFqg5wAuFk", from="youtube", title="title"),
+            Video(key="AkFqg5wAuFk", from="youtube", title="title"),
+            Video(key="AkFqg5wAuFk", from="youtube", title="title")
+        )
+
+        videosAdapter.setData(mockVideos)
+    }
     private fun setVideo() {
         requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-        youtubePlayerView.enterFullScreen()
+
     }
 
 
