404c1f234c88a54d4c2c635fcbfc850c7b4b32d5
Matheus Reis <matheus.reis081@gmail.com>
2023-02-11 17:38:34 -0300

    feat: created click listener to go to youtube

--- app/src/main/java/com/matheusxreis/moviedroid/bindingadapters/VideoRowBinding.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/bindingadapters/VideoRowBinding.kt
@@ -1,5 +1,7 @@
 package com.matheusxreis.moviedroid.bindingadapters
 
+import android.content.Intent
+import android.net.Uri
 import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
@@ -7,6 +9,7 @@ import coil.load
 import com.facebook.shimmer.Shimmer
 import com.facebook.shimmer.ShimmerDrawable
 import com.matheusxreis.moviedroid.R
+import com.matheusxreis.moviedroid.utils.Constants
 
 class VideoRowBinding {
 
@@ -26,11 +29,14 @@ class VideoRowBinding {
                 setShimmer(shimmer)
             }
 
-            val url = "https://i.ytimg.com/vi/${videoKey}/maxresdefault.jpg"
+            val url = "https://i.ytimg.com/vi/${videoKey}/hqdefault.jpg"
             imageView.load(url) {
-
                 error(R.drawable.no_result)
                 placeholder(shimmerDrawable)
+            }
+            imageView.setOnClickListener {
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse("${Constants.YOUTUBE_BASE_URL}${videoKey}"))
+                imageView.context.startActivity(intent)
             }
         }
 
