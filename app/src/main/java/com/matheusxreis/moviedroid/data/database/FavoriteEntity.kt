2eca838241baeddf3012ae8b60914193c8dbe384
Matheus Reis <matheus.reis081@gmail.com>
2023-02-12 09:42:29 -0300

    refactor: added some necessary things in database classes

--- app/src/main/java/com/matheusxreis/moviedroid/data/database/FavoriteEntity.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/data/database/FavoriteEntity.kt
@@ -8,11 +8,14 @@ import com.matheusxreis.moviedroid.utils.Constants
 @Entity(tableName=Constants.TABLE_FAVORITES_NAME)
 data class FavoriteEntity(
     @PrimaryKey(autoGenerate = true)
-    val id: String,
+    val id: Int = 0,
 
     val title: String,
     val itemId: String,
     val imageUrl:String,
     val overview: String,
-    val type: String
+    val type: String,
+    val rating:Float,
+
+    val listCode: String = "favorites"
 )
--- app/src/main/java/com/matheusxreis/moviedroid/data/database/ItemDao.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/data/database/ItemDao.kt
@@ -1,16 +1,17 @@
 package com.matheusxreis.moviedroid.data.database
 
+import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
-
+@Dao
 interface ItemDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertFavoriteItem(favoriteEntity: FavoriteEntity)
 
     @Query("SELECT * FROM favorites_table ORDER BY id")
-    suspend fun readFavoriteItem(): Flow<List<FavoriteEntity>>
+    fun readFavoriteItem(): Flow<List<FavoriteEntity>>
 }
