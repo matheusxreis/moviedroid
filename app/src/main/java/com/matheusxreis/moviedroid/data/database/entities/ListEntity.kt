59d6600504a3688989b198281cef7dc1d35c9372
Matheus Reis <matheus.reis081@gmail.com>
2023-02-13 16:26:47 -0300

    feat: parcelizing list entity to use as argument

--- app/src/main/java/com/matheusxreis/moviedroid/data/database/entities/ListEntity.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/data/database/entities/ListEntity.kt
@@ -1,10 +1,13 @@
 package com.matheusxreis.moviedroid.data.database.entities
 
+import android.os.Parcelable
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.matheusxreis.moviedroid.utils.Constants
+import kotlinx.android.parcel.Parcelize
 import java.util.Date
 
+@Parcelize
 @Entity(tableName = Constants.TABLE_LISTS_NAME)
 data class ListEntity (
     @PrimaryKey(autoGenerate = true)
@@ -13,4 +16,4 @@ data class ListEntity (
     val createdAt: Long = Date().time,
     val coverUrl: String,
     val amountItems:Int = 0
-    )
+    ):Parcelable
