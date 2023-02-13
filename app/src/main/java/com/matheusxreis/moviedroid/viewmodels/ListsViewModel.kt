168e5fe33324c082f57e3d6a02dd4ae7ae75d70d
Matheus Reis <matheus.reis081@gmail.com>
2023-02-13 10:51:15 -0300

    feat: created lists view model

---
+++ app/src/main/java/com/matheusxreis/moviedroid/viewmodels/ListsViewModel.kt
@@ -0,0 +1,32 @@
+package com.matheusxreis.moviedroid.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import androidx.lifecycle.viewModelScope
+import com.matheusxreis.moviedroid.data.Repository
+import com.matheusxreis.moviedroid.data.database.entities.ListEntity
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ListsViewModel @Inject constructor(
+    private val repository: Repository
+):ViewModel() {
+
+
+    val lists = repository.localDataSource.readLists().asLiveData()
+
+
+    fun addList(name:String) = viewModelScope.launch(Dispatchers.IO){
+
+        val newList = ListEntity(
+            name = name,
+            coverUrl = ""
+        )
+
+        repository.localDataSource.insertList(newList)
+    }
+
+}
