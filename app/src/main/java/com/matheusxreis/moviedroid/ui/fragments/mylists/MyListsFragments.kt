75b1f87a110f5ad63c1613b779b27156c3cd38ff
Matheus Reis <matheus.reis081@gmail.com>
2023-02-13 08:57:14 -0300

    feat: created my lists menu with add option

--- app/src/main/java/com/matheusxreis/moviedroid/ui/fragments/mylists/MyListsFragments.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/ui/fragments/mylists/MyListsFragments.kt
@@ -1,13 +1,15 @@
 package com.matheusxreis.moviedroid.ui.fragments.mylists
 
 import android.os.Bundle
+import android.view.*
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.core.view.MenuProvider
+import androidx.core.view.forEach
+import androidx.lifecycle.Lifecycle
 import com.matheusxreis.moviedroid.R
 
-class MyListsFragments : Fragment() {
+class MyListsFragments : Fragment(), MenuProvider {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +21,28 @@ class MyListsFragments : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        requireActivity().addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
         return inflater.inflate(R.layout.fragment_my_lists_fragments, container, false)
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_my_list_fragment, menu)
+
+        menu.forEach {
+            setTintIconMenuItem(it)
+        }
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+       return true
+    }
+
+
+    // CUSTOM FUNCTIONS
+
+    private fun setTintIconMenuItem(menuItem: MenuItem, color: Int = R.color.white) {
+        menuItem.icon?.setTint(ContextCompat.getColor(requireActivity(), color))
     }
 
 }
