16ee0a8a0224695f46af4f1f73b562450b0472a4
Matheus Reis <matheus.reis081@gmail.com>
2023-02-15 13:13:42 -0300

    feat: populating bottom sheet recycler view

--- app/src/main/java/com/matheusxreis/moviedroid/ui/fragments/details/mylistbottomsheet/MyListsBottomSheet.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/ui/fragments/details/mylistbottomsheet/MyListsBottomSheet.kt
@@ -5,11 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.matheusxreis.moviedroid.R
+import com.matheusxreis.moviedroid.adapters.ListsBottomSheetAdapter
+import com.matheusxreis.moviedroid.viewmodels.ListsViewModel
+import kotlinx.android.synthetic.main.fragment_my_lists_bottom_sheet.*
 
 
 class MyListsBottomSheet : BottomSheetDialogFragment() {
+
+    private val mAdapter: ListsBottomSheetAdapter by lazy {
+        ListsBottomSheetAdapter()
+    }
+    private val myListsViewModel:ListsViewModel by activityViewModels<ListsViewModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,9 +31,33 @@ class MyListsBottomSheet : BottomSheetDialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-
-
         return inflater.inflate(R.layout.fragment_my_lists_bottom_sheet, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpRecyclerView()
+        populateRecyclerView()
+
+    }
+
+
+    // CUSTOM FUNCTIONS
+
+    fun setUpRecyclerView(){
+
+        bottomSheetRecyclerView.adapter = mAdapter
+        bottomSheetRecyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+
+    }
+    fun populateRecyclerView(){
+        myListsViewModel.lists.observe(viewLifecycleOwner){
+           if(it.size>1) {
+               val data = it.slice(1..it.lastIndex)
+               mAdapter.setData(data)
+           }
+        }
     }
 
 }
