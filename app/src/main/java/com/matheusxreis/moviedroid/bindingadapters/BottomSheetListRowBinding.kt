c0a74dc633086a492618c2be6ad1877bc2db3db7
Matheus Reis <matheus.reis081@gmail.com>
2023-02-22 12:37:31 -0300

    feat: converted already on list binding adapter to checkbox use

--- app/src/main/java/com/matheusxreis/moviedroid/bindingadapters/BottomSheetListRowBinding.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/bindingadapters/BottomSheetListRowBinding.kt
@@ -2,10 +2,14 @@ package com.matheusxreis.moviedroid.bindingadapters
 
 import android.util.Log
 import android.view.View
+import android.widget.CheckBox
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.findViewTreeLifecycleOwner
+import com.google.android.material.checkbox.MaterialCheckBox
+import com.matheusxreis.moviedroid.R
 import com.matheusxreis.moviedroid.data.database.entities.ListEntity
 import com.matheusxreis.moviedroid.models.MoviePoster
 import com.matheusxreis.moviedroid.viewmodels.ListsViewModel
@@ -22,21 +26,43 @@ class BottomSheetListRowBinding {
         )
         @JvmStatic
         fun alreadyOnList(
-            textView: TextView,
+            view: View,
             listsViewModel: ListsViewModel,
             list: ListEntity,
             movie: MoviePoster
         ) {
             val items = listsViewModel.readListItem(list.id.toString())
 
-            items.observe(textView.findViewTreeLifecycleOwner()!!) { listItems ->
+            items.observe(view.findViewTreeLifecycleOwner()!!) { listItems ->
                 val isOnList = listItems?.find { it.itemId == movie.id }
-                if (isOnList == null) {
-                    textView.visibility = View.INVISIBLE
-                } else {
-                    textView.visibility = View.VISIBLE
+                Log.d("oops", view.toString())
+                when (view) {
+                    is CheckBox -> {
+                        Log.d("oops", "i'm not a text view")
+                        view.isChecked = isOnList != null
+                        view.isEnabled = isOnList != null
+                        view.isClickable = isOnList != null
+
+                        if (isOnList != null) {
+                            view.buttonTintList =
+                                ContextCompat.getColorStateList(view.context, R.color.placeholder)
+                        }
+
+                    }
+
+
+                    is TextView -> {
+                        if (isOnList == null) {
+                            view.visibility = View.INVISIBLE
+                        } else {
+                            view.visibility = View.VISIBLE
+                        }
+                    }
+
+
                 }
             }
+
 
         }
     }
