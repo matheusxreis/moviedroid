791532462eec98f847d0d14f4f086278866efeec
Matheus Reis <matheus.reis081@gmail.com>
2023-02-23 12:05:05 -0300

    feat: populating rv of my list content

--- app/src/main/java/com/matheusxreis/moviedroid/ui/fragments/mylistscontent/MyListsContent.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/ui/fragments/mylistscontent/MyListsContent.kt
@@ -5,10 +5,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.matheusxreis.moviedroid.R
+import com.matheusxreis.moviedroid.adapters.ContentListAdapter
+import com.matheusxreis.moviedroid.viewmodels.ListsViewModel
+import kotlinx.android.synthetic.main.fragment_my_lists_content.*
 
 class MyListsContent : Fragment() {
 
+    val mAdapter: ContentListAdapter by lazy {
+        ContentListAdapter()
+    }
+    val myListsViewModel:ListsViewModel by activityViewModels<ListsViewModel>()
+    val args by navArgs<MyListsContentArgs>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +32,25 @@ class MyListsContent : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_my_lists_content, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpRecyclerView()
+        populateRecyclerView()
+    }
+
+    private fun setUpRecyclerView(){
+        contentListRecyclerView.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        contentListRecyclerView.adapter = mAdapter
+    }
+    private fun populateRecyclerView(){
+        myListsViewModel.readListItem(args.listId.toString()).observe(viewLifecycleOwner){
+            if(!it.isNullOrEmpty()){
+                mAdapter.setData(it)
+            }
+        }
     }
 
 
