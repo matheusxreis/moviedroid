527ec20f28e4c480ae340cdd968222832c4f3073
Matheus Reis <matheus.reis081@gmail.com>
2023-02-23 10:51:36 -0300

    refactor: moved a binding adapter to right file

--- app/src/main/java/com/matheusxreis/moviedroid/bindingadapters/BottomSheetBinding.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/bindingadapters/BottomSheetBinding.kt
@@ -1,5 +1,7 @@
 package com.matheusxreis.moviedroid.bindingadapters
 
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
 
@@ -7,14 +9,7 @@ class BottomSheetBinding {
 
     companion object {
 
-        @BindingAdapter("setAmountItems")
-        @JvmStatic
-        fun setAmountItems(textView: TextView, amount:Int){
-            if(amount != 1){
-                textView.text = "$amount items"
-            }else {
-                textView.text = "1 item"
-            }
-        }
+
+
     }
 }
--- app/src/main/java/com/matheusxreis/moviedroid/bindingadapters/BottomSheetListRowBinding.kt
+++ app/src/main/java/com/matheusxreis/moviedroid/bindingadapters/BottomSheetListRowBinding.kt
@@ -67,5 +67,15 @@ class BottomSheetListRowBinding {
 
 
         }
+
+        @BindingAdapter("setAmountItems")
+        @JvmStatic
+        fun setAmountItems(textView: TextView, amount:Int){
+            if(amount != 1){
+                textView.text = "$amount items"
+            }else {
+                textView.text = "1 item"
+            }
+        }
     }
 }
